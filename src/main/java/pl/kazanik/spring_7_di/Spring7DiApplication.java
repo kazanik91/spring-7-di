76d43902087f95d7a67cb0b5dbbb15f786951f30
miron.maksymiuk <miron.maksymiuk@OSZ1-LDL-P31722.synapse.com>
2026-02-27 22:02:11 +0100

    Created new Spring Boot 4 project using Spring Initializr website. Project is Spring Boot 4, Java 17 and Maven, no extra dependencies.

---
+++ src/main/java/pl/kazanik/spring_7_di/Spring7DiApplication.java
@@ -0,0 +1,13 @@
+package pl.kazanik.spring_7_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7DiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kazanik/spring_7_di/Spring7DiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kazanik.spring_7_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7DiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
