80c33ad3de4d8d6cafae9c372c5d6c2fe70c5f39
miron.maksymiuk <miron.maksymiuk@OSZ1-LDL-P31722.synapse.com>
2026-02-27 22:59:22 +0100

    Added simple spring context and dependency injection tests using SpringBootTest.

--- src/test/java/pl/kazanik/spring_7_di/Spring7DiApplicationTests.java
+++ src/test/java/pl/kazanik/spring_7_di/Spring7DiApplicationTests.java
@@ -1,13 +1,33 @@
 package pl.kazanik.spring_7_di;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import pl.kazanik.spring_7_di.controller.MyController;
 
 @SpringBootTest
 class Spring7DiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ApplicationContext ctx;
+    
+    @Autowired
+    private MyController myController;
+    
+    @Test
+    void contextLoads() {
+    }
+    
+    @Test
+    void testControllerFromAppCtx() {
+        MyController controller = ctx.getBean(MyController.class);
+        System.out.println(controller.sayHello());
+    }
+    
+    @Test
+    void testControllerAutowired() {
+        System.out.println(this.myController.sayHello());
+    }
 
 }
